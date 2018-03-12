12a93f6d5b86cbc755b90b184e389b1d014fa29e
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2018-03-12 18:18:39 -0400

    made small changes for css transition

--- src/menuClick.js
+++ src/menuClick.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementsByClassName("toggle")[0].addEventListener("click", function () {
-        var ul = document.getElementsByTagName("ul")[0];
-        if (ul.getAttribute("class") !== "active") {
-            ul.setAttribute("class", "active");
+        var nb = document.getElementById("navBar");
+        if (nb.getAttribute("class") !== "active") {
+            nb.setAttribute("class", "active");
         } else {
-            ul.removeAttribute("class");
+            nb.removeAttribute("class");
         }
     });
 });
+
+// function resizePage(){
+//     var element, style;
+//     elem = document.querySelector('.active');
+//     style = getComputedStyle(elem);
+//     var width = (window.innerWidth > 0) ? window.innerWidth : document.documentElement.clientWidth;
+//     if(width >= 950 && width <= 951){ // your page breakpoint happens probably between 525px and 526px width at this range we gonna disable the transition temporarily
+//         style.transitionProperty="none";
+//     } 
+//     else{
+//         style.transitionProperty="visibility, opacity, transform";
+//     }
+// }
